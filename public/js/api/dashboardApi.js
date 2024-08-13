584b2bbad80dfa686854aa5371df42cde9117c12
Jeetendrasinh <jeetendraparmar22@gmail.com>
2024-08-13 07:37:02 +0530

    handle js error

--- public/js/api/dashboardApi.js
+++ public/js/api/dashboardApi.js
@@ -8,7 +8,9 @@ async function patients() {
             responseData.forEach((patient) => {
                 const patientRow = ` <tr>
                                 <td>${patient.updated_date_ist}</td>
-                <td><a href="/patient-details/${patient.id}">${patient.full_name}</a></td>
+                <td><a href="/patient-details/${patient.id}">${
+                    patient.full_name
+                }</a></td>
                 <td><div class="spinner-grow text-danger" role="status">
   <span class="visually-hidden">Loading...</span>
 </div></td>
@@ -16,7 +18,9 @@ async function patients() {
                 <td>${patient.phone}</td>            
                 <td>${patient.phone}</td>
                 <td><i class="fa fa-map-marker" style="font-size:30px;color:red"></i></td>
-                <td>${patient.latest_battery_percentage.value} %</td>
+                <td>${
+                    patient.latest_battery_percentage?.value || "No Data"
+                } %</td>
                 <td>${patient.watch_data.connection_status}</td>
 
                 
